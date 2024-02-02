654e8d317c2e739e73ffc8c90814248661e15015
SosisterRapStar <timoshenkovvanya@gmail.com>
2024-02-02 20:57:15 +0300

    alembic added

--- src/models/chat_models.py
+++ src/models/chat_models.py
@@ -1,5 +1,5 @@
 from sqlalchemy import String
-from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import Mapped, relationship
 from .base import Base
 from .annotated_types import *
 
@@ -10,6 +10,7 @@ class Convesation(Base):
     status: Mapped[str] = mapped_column(nullable=False)
     is_group: Mapped[bool] = mapped_column(nullable=False)
     name: Mapped[str | None]
+    users: Mapped[list["User"]] = relationship(back_populates="conversations", uselist=True)
     created_at: Mapped[created_at_timestamp]
     updated_at: Mapped[updated_at_timestamp]
 
@@ -31,6 +32,8 @@ class User(Base):
 
     name: Mapped[str] = mapped_column(String(20), nullable=False)
     email: Mapped[str] = mapped_column(nullable=False)
+    conversations: Mapped[list["Convesation"]] = relationship(back_populates="users", uselist=True)
+
     hashed_password: Mapped[str]
     is_online: Mapped[bool] = mapped_column(nullable=False)
     created_at: Mapped[created_at_timestamp]
