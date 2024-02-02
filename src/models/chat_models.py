db1387edabe21f6541bf395a97a7ec3d10a3e437
SosisterRapStar <timoshenkovvanya@gmail.com>
2024-02-02 22:01:20 +0300

    added relationships
    But: there are some problems with lazy loading with Async

--- src/models/chat_models.py
+++ src/models/chat_models.py
@@ -1,7 +1,34 @@
-from sqlalchemy import String
+from sqlalchemy import String, ForeignKey
 from sqlalchemy.orm import Mapped, relationship
 from .base import Base
 from .annotated_types import *
+
+
+class User(Base):
+    __tablename__ = "user"
+
+    name: Mapped[str] = mapped_column(String(20), nullable=False)
+    email: Mapped[str] = mapped_column(nullable=False)
+    conversations: Mapped[list["Convesation"]] = relationship(
+        back_populates="users",
+        uselist=True,
+        secondary="user_conversation",
+    )
+    messages: Mapped[list['Message']] = relationship(back_populates='user', uselist=True)
+    hashed_password: Mapped[str]
+    is_online: Mapped[bool] = mapped_column(nullable=False)
+    created_at: Mapped[created_at_timestamp]
+
+
+class Message(Base):
+    __tablename__ = "messages"
+
+    content: Mapped[str] = mapped_column(nullable=False)
+    status: Mapped[str] = mapped_column(nullable=False)
+    user: Mapped["User"] = relationship(back_populates='messages', uselist=False)
+    user_fk: Mapped[UUIDpk] = mapped_column(ForeignKey("user.id"))
+    created_at: Mapped[created_at_timestamp]
+    updated_at: Mapped[updated_at_timestamp]
 
 
 class Convesation(Base):
@@ -10,30 +37,17 @@ class Convesation(Base):
     status: Mapped[str] = mapped_column(nullable=False)
     is_group: Mapped[bool] = mapped_column(nullable=False)
     name: Mapped[str | None]
-    users: Mapped[list["User"]] = relationship(back_populates="conversations", uselist=True)
+    users: Mapped[list["User"]] = relationship(
+        back_populates="conversations",
+        uselist=True,
+        secondary="user_conversation",
+    )
     created_at: Mapped[created_at_timestamp]
     updated_at: Mapped[updated_at_timestamp]
 
 
-
-class Message(Base):
-    __tablename__ = "messages"
-
-    content: Mapped[str] = mapped_column(nullable=False)
-    status: Mapped[str] = mapped_column(nullable=False)
-    created_at: Mapped[created_at_timestamp]
-    updated_at: Mapped[updated_at_timestamp]
-
-
-
-
-class User(Base):
-    __tablename__ = "user"
-
-    name: Mapped[str] = mapped_column(String(20), nullable=False)
-    email: Mapped[str] = mapped_column(nullable=False)
-    conversations: Mapped[list["Convesation"]] = relationship(back_populates="users", uselist=True)
-
-    hashed_password: Mapped[str]
-    is_online: Mapped[bool] = mapped_column(nullable=False)
-    created_at: Mapped[created_at_timestamp]
+class UserConversationSecondary(Base):
+    __tablename__ = "user_conversation"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_fk = mapped_column(ForeignKey("user.id"))
+    conversation_fk = mapped_column(ForeignKey("conversation.id"))
