d4c9f3ebc760ef599606738eed89613d2fa94dd5
SosisterRapStar <timoshenkovvanya@gmail.com>
2024-02-13 23:41:00 +0300

    another one commit

--- src/crud/user_repository.py
+++ src/crud/user_repository.py
@@ -1,9 +1,19 @@
-# from src.crud.repo_abstract import CRUDAlchemyRepository
-# from src.models.user_model import User
-#
-#
-# class UserRepository(CRUDAlchemyRepository):
-#     model = User
-#
-#     async def get_user_with_convs(self):
-#         select
+from select import select
+
+from src.crud.repo_abstract import CRUDAlchemyRepository
+from src.models.chat_models import Conversation, UserConversationSecondary
+from src.models.user_model import User
+
+
+class UserRepository(CRUDAlchemyRepository):
+    model = User
+
+    async def get_user_with_convs(self, user_id) -> list[Conversation]:
+        stmt = (
+            select(Conversation)
+            .join(User, User.id == UserConversationSecondary.user_id)
+            .join(Conversation, Conversation.id == UserConversationSecondary.conversation_id)
+            .where(User.id == user_id)
+        )
+        convs = await self.session.scalars(stmt)
+        return list(convs.all())
--- src/utils.py
+++ src/utils.py
@@ -1,18 +1,29 @@
 import asyncio
 
+
+
 from src.crud.demo_relational_crud import get_user_convs
 
 # from src.crud.conversation_crud import *
 from src.crud.user_crud import *
 from src.crud.utils import get_object
 from src.models import db_handler
+from src.models.chat_models import Conversation, UserConversationSecondary
 
 
 #
 async def main():
     async with db_handler.session() as ses:
-        a = await get_object(async_session=ses, model=User, id="9f3454e3-e6c3-4501-809f-c8a38ceef2ed")
-        print(a)
+        stmt = (
+            select(Conversation)
+            .join(UserConversationSecondary, Conversation.id == UserConversationSecondary.conversation_id)
+            .join(User, User.id == UserConversationSecondary.user_id)
+            .where(User.name == "")
+        )
+        res = await ses.scalars(stmt)
+        a = list(res.all())[0]
+        print(a.user_admin_fk)
+        print(stmt)
 
 
 if __name__ == "__main__":
