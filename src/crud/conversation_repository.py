e56968fd16ba7d0401a6d9f5aaabc6e318bf98a2
SosisterRapStar <timoshenkovvanya@gmail.com>
2024-02-17 22:30:02 +0300

    Create returns new object

--- src/crud/conversation_repository.py
+++ src/crud/conversation_repository.py
@@ -1,6 +1,6 @@
 import uuid
 
-from sqlalchemy import select
+from sqlalchemy import select, insert
 from sqlalchemy.orm import joinedload
 
 from src.crud.repo_abstract import CRUDAlchemyRepository
@@ -14,12 +14,12 @@ class ConversationRepository(CRUDAlchemyRepository):
     # TODO: do something with session identity map for caching
     # TODO: errors handling
 
-    async def get_conv_users(self, conv_id: uuid.UUID) -> list["User"]:
+    async def get_users(self, conv_id: uuid.UUID) -> list["User"]:
         stmt = (
             select(User)
             .join(
                 UserConversationSecondary,
-                Conversation.id == UserConversationSecondary.conversation_id,
+                User.id == UserConversationSecondary.user_id,
             )
             .where(UserConversationSecondary.conversation_id == conv_id)
         )
@@ -49,3 +49,12 @@ class ConversationRepository(CRUDAlchemyRepository):
         )
         conv = await self._session.scalar(stmt)
         return conv
+
+    async def create(self, data: dict) -> Conversation:
+        new_conv = Conversation(**data)
+        new_asoc = UserConversationSecondary(edit_permission=True)
+        new_asoc.conversation_id = new_conv.id
+        new_asoc.user_id = new_conv.user_admin_fk
+        self._session.add(new_conv)
+        await self._session.commit()
+        return new_conv
