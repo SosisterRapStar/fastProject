3e148b9969bf2fe1d326c772b98b5ba356833125
SosisterRapStar <timoshenkovvanya@gmail.com>
2024-02-18 01:56:58 +0300

    Ha, ha, ha, schyeah

--- src/routers/conversations.py
+++ src/routers/conversations.py
@@ -1,12 +1,14 @@
 import uuid
-from typing import List, Annotated
-from fastapi import APIRouter, Path, Depends, HTTPException
-from sqlalchemy.ext.asyncio import AsyncSession
-
-from src.crud.conversation_repository import ConversationRepository
-from src.dependencies.hash_dependency import User_on_request_hash_dep
-from src.dependencies.session_dependency import session_dep
-from src.schemas.conversation import ConversationResponse, ConversationRequest, ConversationUpdate
+from typing import List
+from fastapi import APIRouter, HTTPException
+from sqlalchemy.exc import IntegrityError
+from src.dependencies.repo_providers_dependency import conv_repo_provider
+from src.schemas.conversation import (
+    ConversationResponse,
+    ConversationRequest,
+    ConversationUpdate,
+    AddUser,
+)
 from src.schemas.user_conversation import ConversationWithUsersResp
 
 from src.schemas.users import User_on_response, User_on_request
@@ -14,23 +16,11 @@ from src.schemas.users import User_on_response, User_on_request
 router = APIRouter(tags=["Conversations"])
 
 
-# TODO: убрать явный объект другой природы (сессию) из dependencies в роутах
-# сессия должна быть инкапсулирована в работе методов crud и других
-# @router.post(
-#     "/{creator_id}/",
-#     response_model=ConversationResponse,
-# )
-# async def demo_create_conversation(creator_id: uuid.UUID, session: session_dep, conversation: ConversationRequest):
-#     return await conversation_crud.create_conversation(conversation_shema=conversation, async_session=session,
-#                                                        user_id=creator_id)
-
-
 @router.get(
     "/",
     response_model=List[ConversationResponse],
 )
-async def get_all_conversations(session: session_dep):
-    conv_repo = ConversationRepository(session=session)
+async def get_all_conversations(conv_repo: conv_repo_provider):
     return await conv_repo.get()
 
 
@@ -38,43 +28,106 @@ async def get_all_conversations(session: session_dep):
     "/create/",
     response_model=ConversationResponse,
 )
-async def create_conversation(session: session_dep, conversation: ConversationRequest):
-    conv_repo = ConversationRepository(session=session)
-    return await conv_repo.create(conversation.model_dump())
+async def create_conversation(
+    conv_repo: conv_repo_provider, conversation: ConversationRequest
+):
+
+    try:
+        return await conv_repo.create(conversation.model_dump())
+    except IntegrityError:
+        raise HTTPException(status_code=404, detail="Admin user not found")
 
 
 @router.get(
     "/users/{conv_id}/",
     response_model=List[User_on_response],
 )
-async def get_all_users_in_conv(conv_id: uuid.UUID, session: session_dep):
-    conv_repo = ConversationRepository(session=session)
-    return await conv_repo.get_users(conv_id=conv_id)
+async def get_all_users_in_conv(conv_id: uuid.UUID, conv_repo: conv_repo_provider):
+
+    try:
+        return await conv_repo.get_users(conv_id=conv_id)
+    except IntegrityError:
+        raise HTTPException(status_code=404, detail="Conversation not found")
 
 
 @router.get("/{conv_name}/", response_model=ConversationResponse)
-async def get_conv_by_name(conv_name: str, session: session_dep):
-    conv_repo = ConversationRepository(session=session)
-    return await conv_repo.get(name=conv_name)
+async def get_conv_by_name(conv_name: str, conv_repo: conv_repo_provider):
+
+    try:
+        return await conv_repo.get(name=conv_name)
+    except IntegrityError:
+        raise HTTPException(status_code=404, detail="Conversation not found")
 
 
 @router.get("/by_id/{conv_id}/", response_model=ConversationResponse)
-async def get_conv_by_id(conv_id: uuid.UUID, session: session_dep):
-    conv_repo = ConversationRepository(session=session)
-    return await conv_repo.get(id=conv_id)
+async def get_conv_by_id(conv_id: uuid.UUID, conv_repo: conv_repo_provider):
+
+    try:
+        return await conv_repo.get(id=conv_id)
+    except IntegrityError:
+        raise HTTPException(status_code=404, detail="Conversation not found")
 
 
-@router.patch("/{conv_id}/", response_model=ConversationResponse, )
-async def update_conv(conv_id: uuid.UUID, session: session_dep, conversation: ConversationUpdate,
-                      ):
-    conv_repo = ConversationRepository(session=session)
-    return await conv_repo.update(conversation.model_dump(exclude_unset=True), id=conv_id)
+@router.patch(
+    "/{conv_id}/",
+    response_model=ConversationResponse,
+)
+async def update_conv(
+    conv_id: uuid.UUID,
+    conv_repo: conv_repo_provider,
+    conversation: ConversationUpdate,
+):
+
+    try:
+        return await conv_repo.update(
+            conversation.model_dump(exclude_unset=True), id=conv_id
+        )
+    except IntegrityError:
+        raise HTTPException(status_code=404, detail="Conversation not found")
 
 
-@router.patch("/by_name/{conv_name/", response_model=ConversationResponse, )
-async def update_conv(conv_name: str,
-                      session: session_dep,
-                      conversation: ConversationUpdate,
-                      ):
-    conv_repo = ConversationRepository(session=session)
-    return await conv_repo.update(conversation.model_dump(exclude_unset=True), name=conv_name)
+@router.patch(
+    "/by_name/{conv_name/",
+    response_model=ConversationResponse,
+)
+async def update_conv(
+    conv_name: str,
+    conv_repo: conv_repo_provider,
+    conversation: ConversationUpdate,
+):
+
+    try:
+        return await conv_repo.update(
+            conversation.model_dump(exclude_unset=True), name=conv_name
+        )
+    except IntegrityError:
+        raise HTTPException(status_code=404, detail="Conversation not found")
+
+
+@router.post("users/{conv_id}/")
+async def add_user_to_conversation(
+    conv_id: uuid.UUID,
+    conv_repo: conv_repo_provider,
+    add_model: AddUser,
+) -> None:
+
+    try:
+        await conv_repo.add_user(
+            user_id=add_model.user_id,
+            permission=add_model.is_moder,
+            conv_id=conv_id,
+        )
+    except IntegrityError:
+        raise HTTPException(
+            status_code=404, detail="User already added or some of id not found"
+        )
+
+
+@router.delete("/{conv_id}/")
+async def delete_conv(conv_id: uuid.UUID, conv_repo: conv_repo_provider):
+    try:
+        return await conv_repo.delete(id=conv_id)
+    except IntegrityError:
+        raise HTTPException(
+            status_code=404, detail="Хз тут все что угодно отъебнуть может"
+        )
--- src/routers/users.py
+++ src/routers/users.py
@@ -1,6 +1,7 @@
 import uuid
 from typing import List, Annotated
 from fastapi import APIRouter, Path, Depends, HTTPException
+from sqlalchemy.exc import IntegrityError
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.crud.user_repository import UserRepository
@@ -8,6 +9,7 @@ from src.dependencies.hash_dependency import (
     User_on_request_hash_dep,
     User_for_update_hash_dep,
 )
+from src.dependencies.repo_providers_dependency import user_repo_provider
 from src.dependencies.session_dependency import session_dep
 from src.schemas.conversation import ConversationResponse
 from src.schemas.user_conversation import UserOnResponseWithConvs
@@ -23,8 +25,7 @@ router = APIRouter(tags=["Users"])
     "/",
     response_model=User_on_response,
 )
-async def create_user(user: User_on_request_hash_dep, session: session_dep):
-    user_repo = UserRepository(session=session)
+async def create_user(user: User_on_request_hash_dep, user_repo: user_repo_provider):
     return await user_repo.create(user.model_dump())
 
 
@@ -32,8 +33,7 @@ async def create_user(user: User_on_request_hash_dep, session: session_dep):
     "/",
     response_model=List[User_on_response],
 )
-async def get_users(session: session_dep):
-    user_repo = UserRepository(session=session)
+async def get_users(user_repo: user_repo_provider):
     return await user_repo.get()
 
 
@@ -41,27 +41,42 @@ async def get_users(session: session_dep):
     "by_name/{user_name}/",
     response_model=User_on_response,
 )
-async def get_user_by_name(user_name: str, session: session_dep):
-    user_repo = UserRepository(session=session)
-    return await user_repo.get(name=user_name)
+async def get_user_by_name(user_name: str, user_repo: user_repo_provider):
+    try:
+        return await user_repo.get(name=user_name)
+    except IntegrityError:
+        raise HTTPException(
+            status_code=404,
+            detail="User not found",
+        )
 
 
 @router.get(
     "/{user_id}/",
     response_model=User_on_response,
 )
-async def get_user_by_id(user_id: uuid.UUID, session: session_dep):
-    user_repo = UserRepository(session=session)
-    return await user_repo.get(id=user_id)
+async def get_user_by_id(user_id: uuid.UUID, user_repo: user_repo_provider):
+    try:
+        return await user_repo.get(id=user_id)
+    except IntegrityError:
+        raise HTTPException(
+            status_code=404,
+            detail="User not found",
+        )
 
 
 @router.get(
     "/convs/{user_id}/",
     response_model=List[ConversationResponse],
 )
-async def get_user_convs_by_id(user_id: uuid.UUID, session: session_dep):
-    user_repo = UserRepository(session=session)
-    return await user_repo.get_convs(user_id=user_id)
+async def get_user_convs_by_id(user_id: uuid.UUID, user_repo: user_repo_provider):
+    try:
+        return await user_repo.get_convs(user_id=user_id)
+    except IntegrityError:
+        raise HTTPException(
+            status_code=404,
+            detail="User not found",
+        )
 
 
 @router.patch(
@@ -69,12 +84,17 @@ async def get_user_convs_by_id(user_id: uuid.UUID, session: session_dep):
     response_model=User_on_response,
 )
 async def update_user_by_id(
-    session: session_dep,
+    user_repo: user_repo_provider,
     user_id: uuid.UUID,
     user: User_for_update_hash_dep,
 ):
-    user_repo = UserRepository(session=session)
-    return await user_repo.update(user.model_dump(exclude_unset=True), id=user_id)
+    try:
+        return await user_repo.update(user.model_dump(exclude_unset=True), id=user_id)
+    except IntegrityError:
+        raise HTTPException(
+            status_code=404,
+            detail="User not found",
+        )
 
 
 @router.patch(
@@ -82,18 +102,30 @@ async def update_user_by_id(
     response_model=User_on_response,
 )
 async def update_user_by_name(
-    session: session_dep,
+    user_repo: user_repo_provider,
     user_name: str,
     user: User_for_update_hash_dep,
 ):
-    user_repo = UserRepository(session=session)
-    return await user_repo.update(user.model_dump(exclude_unset=True), name=user_name)
+    try:
+        return await user_repo.update(
+            user.model_dump(exclude_unset=True), name=user_name
+        )
+    except IntegrityError:
+        raise HTTPException(
+            status_code=404,
+            detail="User not found",
+        )
 
 
 @router.delete("/{user_id}/")
 async def delete_user_by_id(
-    session: session_dep,
+    user_repo: user_repo_provider,
     user_id: uuid.UUID,
 ):
-    user_repo = UserRepository(session=session)
-    return await user_repo.delete(id=user_id)
+    try:
+        return await user_repo.delete(id=user_id)
+    except IntegrityError:
+        raise HTTPException(
+            status_code=404,
+            detail="User not found",
+        )
