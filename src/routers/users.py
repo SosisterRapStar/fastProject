0fc1e94dd625fec49d98829370ce02f1d34d95ee
SosisterRapStar <timoshenkovvanya@gmail.com>
2024-02-17 17:50:32 +0300

    Rewrited routers

--- src/routers/users.py
+++ src/routers/users.py
@@ -3,24 +3,26 @@ from typing import List, Annotated
 from fastapi import APIRouter, Path, Depends, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from src.crud.user_crud import load_user_with_convs
+from src.crud.user_repository import UserRepository
 from src.dependencies.get_dependency import get_by_id_dep, get_by_name
 from src.dependencies.hash_dependency import User_on_request_hash_dep
 from src.dependencies.session_dependency import session_dep
+from src.schemas.conversation import ConversationResponse
 from src.schemas.user_conversation import UserOnResponseWithConvs
-
 from src.schemas.users import User_on_response, User_on_request
-from src.crud import user_crud
+
 
 router = APIRouter(tags=["Users"])
 
+#TODO: чтобы каждый раз не создавать объект репозиториев вынести эту логику в отдельные классы
 
 @router.post(
     "/",
     response_model=User_on_response,
 )
 async def create_user(user: User_on_request_hash_dep, session: session_dep):
-    return await user_crud.create_user(async_session=session, user=user)
+    user_repo = UserRepository(session=session)
+    return await user_repo.create(user.model_dump())
 
 
 @router.get(
@@ -29,14 +31,17 @@ async def create_user(user: User_on_request_hash_dep, session: session_dep):
     response_model=List[User_on_response],
 )
 async def get_users(session: session_dep):
-    return await user_crud.get_users(async_session=session)
+    user_repo = UserRepository(session=session)
+    return await user_repo.get()
 
 @router.get(
     "by_name/{user_name}/",
     response_model=User_on_response,
 )
-async def get_user_by_name(user: get_by_name):
-    return user
+async def get_user_by_name(user_name: str, session: session_dep):
+    user_repo = UserRepository(session=session)
+    return await user_repo.get(name=user_name)
+
 
 
 @router.get(
@@ -44,25 +49,24 @@ async def get_user_by_name(user: get_by_name):
     tags=["user"],
     response_model=User_on_response,
 )
-async def get_user_by_id(user: get_by_id_dep):
-    return user
+async def get_user_by_id(user_id: uuid.UUID, session: session_dep):
+    user_repo = UserRepository(session=session)
+    return await user_repo.get(id=user_id)
 
 @router.get(
     "/convs/{user_id}/",
-    response_model=UserOnResponseWithConvs,
+    response_model=ConversationResponse,
 )
-async def get_user_by_id(user_id: uuid.UUID, session: session_dep):
-    return await load_user_with_convs(user_id=user_id, async_session=session)
+async def get_user_convs_by_id(user_id: uuid.UUID, session: session_dep):
+    user_repo = UserRepository(session=session)
+    return await user_repo.get_convs(user_id=user_id)
 
 
 @router.patch("/{user_id}/", response_model=User_on_response)
 async def update_user_by_id(
-        user_db: get_by_id_dep,
         session: session_dep,
+        user_id: uuid.UUID,
         user: User_on_request_hash_dep,
 ):
-    return await user_crud.update_user(
-        async_session=session,
-        user_db=user_db,
-        user_update=user,
-    )
+    user_repo = UserRepository(session=session)
+    return await user_repo.update(user.model_dump(exclude_unset=True), id=user_id)
