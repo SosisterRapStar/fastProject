3ed172e1350efee7486cfdf677e00181aacc7d68
SosisterRapStar <timoshenkovvanya@gmail.com>
2024-02-18 14:37:15 +0300

    ds

--- src/schemas/conversation.py
+++ src/schemas/conversation.py
@@ -9,6 +9,8 @@ from ..config import schemas_settings
 
 class ConversationRequestBase(BaseModel):
     model_config = ConfigDict(extra=schemas_settings.extra)
+
+
 class ConversationBase(BaseModel):
     name: str = Field(max_length=20)
 
