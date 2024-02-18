a3818b26408733ef96443effa0395b2055477de9
SosisterRapStar <timoshenkovvanya@gmail.com>
2024-02-18 14:29:19 +0300

    Extra data in JSON is forbidden

--- src/schemas/conversation.py
+++ src/schemas/conversation.py
@@ -4,13 +4,16 @@ from typing import TYPE_CHECKING
 from pydantic import BaseModel, EmailStr, Field, UUID4, ConfigDict
 import uuid
 from .users import User_on_response
+from ..config import schemas_settings
 
 
+class ConversationRequestBase(BaseModel):
+    model_config = ConfigDict(extra=schemas_settings.extra)
 class ConversationBase(BaseModel):
     name: str = Field(max_length=20)
 
 
-class ConversationRequest(ConversationBase):
+class ConversationRequest(ConversationBase, ConversationRequestBase):
     user_admin_fk: uuid.UUID
 
 
@@ -20,7 +23,7 @@ class ConversationResponse(ConversationBase):
     user_admin_fk: uuid.UUID
 
 
-class ConversationUpdate(ConversationBase):
+class ConversationUpdate(ConversationBase, ConversationRequestBase):
     name: str = Field(max_length=20, default=None)
 
 
--- src/schemas/users.py
+++ src/schemas/users.py
@@ -3,9 +3,14 @@ from typing import Annotated, TYPE_CHECKING
 from pydantic import BaseModel, EmailStr, Field, UUID4, ConfigDict
 import uuid
 
+from ..config import schemas_settings
 
 if TYPE_CHECKING:
     from .conversation import ConversationResponse
+
+
+class BaseRequestUser(BaseModel):
+    model_config = ConfigDict(extra=schemas_settings.extra)
 
 
 class BaseUser(BaseModel):
@@ -16,7 +21,7 @@ class BaseUser(BaseModel):
     email: EmailStr
 
 
-class User_on_request(BaseUser):
+class User_on_request(BaseUser, BaseRequestUser):
     password: str | None = Field(
         max_length=20,
         title="User password",
@@ -24,7 +29,7 @@ class User_on_request(BaseUser):
     )
 
 
-class User_for_update(BaseUser):
+class User_for_update(BaseUser, BaseRequestUser):
     name: str = Field(title="Name of the user", max_length=20, default=None)
     email: EmailStr | None = None
     password: str | None = None
