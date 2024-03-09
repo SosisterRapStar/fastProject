ecfc58cb77a16e4ff976a1dced8fde80a8472094
SosisterRapStar <timoshenkovvanya@gmail.com>
2024-03-10 01:03:36 +0300

    Refactor

--- src/main.py
+++ src/main.py
@@ -25,45 +25,6 @@ from middlewares.log_middleware import ASGIMiddleware
 
 middleware = [Middleware(ASGIMiddleware)]
 
-html = """
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Chat</title>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/websocket-client/1.2.0/websocket.min.js"></script>
-</head>
-<body>
-<h1>WebSocket Chat</h1>
-<form action="" onsubmit="sendMessage(event)">
-    <input type="text" id="messageText" autocomplete="off"/>
-    <button>Send</button>
-</form>
-<ul id='messages'>
-</ul>
-<script>
-    var convId = window.location.pathname.split('/')[1];
-      // Replace with your actual access token
-    var ws = new WebSocket("ws://localhost:8000/" + convId + "/ws");
-    ws.onopen = function(event) {
-        ws.send(JSON.stringify({ 'Authorization': 'Bearer ' + token }));
-    };
-    ws.onmessage = function(event) {
-        var messages = document.getElementById('messages')
-        var message = document.createElement('li')
-        var content = document.createTextNode(event.data)
-        message.appendChild(content)
-        messages.appendChild(message)
-    };
-    function sendMessage(event) {
-        var input = document.getElementById("messageText")
-        ws.send(input.value)
-        input.value = ''
-        event.preventDefault()
-    }
-</script>
-</body>
-</html>
-"""
 
 config = setup_logging()
 log = logging.getLogger("__name__")
@@ -80,41 +41,6 @@ app.include_router(
 
 
 # Testing
-@app.get("/{conv_id}")
-async def get(
-    current_user: get_current_user,
-    conv_id: uuid.UUID,
-    conv_repo: conv_repo_provider,
-):
-    # await conv_repo.get_conv_messages(conv_id=conv_id)
-    return HTMLResponse(html)
-
-
-# Testing. Acces_token header added for testing in /docs
-@app.websocket("/{conv_id}/ws")
-async def websocket_endpoint(
-    current_user: get_current_user_ws,
-    conv_id: uuid.UUID,
-    websocket: WebSocket,
-    message_repo: message_repo_provider,
-):
-    manager = await conv_managers_handler.get_manager(key=conv_id)
-    await manager.connect(websocket)
-    try:
-        while True:
-            data = await websocket.receive_text()
-            message_data = {
-                "conversation_fk": conv_id,
-                "content": data,
-                "user_fk": current_user.id,
-            }
-            await message_repo.create(message_data)
-            await manager.broadcast(f"Clients {current_user.name}: {data}")
-    except WebSocketDisconnect:
-        manager.disconnect(websocket)
-        await manager.broadcast(
-            f"Clients {current_user.name}: left the chat"
-        )
 
 
 if __name__ == "__main__":
--- src/routers/__init__.py
+++ src/routers/__init__.py
@@ -4,6 +4,7 @@ from .users import router as user_router
 from .conversations import router as conversation_router
 from .authorization import router as auth_router
 from .messages import router as message_router
+from .chat import router as chat_router
 
 
 router = APIRouter()
@@ -12,3 +13,5 @@ router.include_router(router=user_router, prefix="/users")
 router.include_router(router=conversation_router, prefix="/conversations")
 router.include_router(router=auth_router, prefix="/auth")
 router.include_router(router=message_router, prefix="/messages")
+
+router.include_router(router=chat_router, prefix="/chat")
