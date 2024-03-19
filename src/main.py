0f6123019752ae004391dfc405bd6c43564c72fc
vanya <timoshenkovvanya@gmail.com>
2024-03-19 22:10:26 +0300

    dss

--- src/main.py
+++ src/main.py
@@ -1,47 +1,63 @@
-import logging
-import uuid
-from typing import Annotated
 
 import uvicorn
-from fastapi import FastAPI, WebSocket, Header
-from starlette.websockets import WebSocketDisconnect
-
+from fastapi import FastAPI, WebSocket
 from routers import router as router_api_v1
 from config import settings
 from fastapi.responses import HTMLResponse
-from logger import setup_logging
-from src.authorization.dependency_auth import get_current_user, get_current_user_ws
-from src.connections.connection_manager import (
-    ConversationConnectionManagersHandler,
-    conv_managers_handler,
-)
-from src.dependencies.repo_providers_dependency import (
-    message_repo_provider,
-    conv_repo_provider,
-)
-from starlette.middleware import Middleware
-from middlewares.log_middleware import ASGIMiddleware
-# from middlewares.auth_middleware import WebSocketAuthMiddleware
 
-middleware = [Middleware(ASGIMiddleware)]
+html = """
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Chat</title>
+    </head>
+    <body>
+        <h1>WebSocket Chat</h1>
+        <form action="" onsubmit="sendMessage(event)">
+            <input type="text" id="messageText" autocomplete="off"/>
+            <button>Send</button>
+        </form>
+        <ul id='messages'>
+        </ul>
+        <script>
+            var ws = new WebSocket("ws://localhost:8000/ws");
+            ws.onmessage = function(event) {
+                var messages = document.getElementById('messages')
+                var message = document.createElement('li')
+                var content = document.createTextNode(event.data)
+                message.appendChild(content)
+                messages.appendChild(message)
+            };
+            function sendMessage(event) {
+                var input = document.getElementById("messageText")
+                ws.send(input.value)
+                input.value = ''
+                event.preventDefault()
+            }
+        </script>
+    </body>
+</html>
+"""
 
-
-config = setup_logging()
-log = logging.getLogger("__name__")
-
-logging.getLogger("sqlalchemy.orm").setLevel(logging.ERROR)
-logging.getLogger("sqlalchemy.engine").setLevel(logging.ERROR)
-logging.getLogger("sqlalchemy.dialects").setLevel(logging.ERROR)
-
-app = FastAPI(middleware=middleware)
+app = FastAPI()
 app.include_router(
     router=router_api_v1,
     prefix=settings.router_settings.api_v1_prefix,
 )
 
 
-# Testing
+@app.get("/")
+async def get():
+    return HTMLResponse(html)
+
+
+# @app.websocket("/ws")
+# async def websocket_endpoint(websocket: WebSocket):
+#     await websocket.accept()
+#     while True:
+#         data = await websocket.receive_text()
+#         await websocket.send_text(f"Pososy pisu: {data}")
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", reload=True, log_config=config)
+    uvicorn.run("app", reload=True, port=8000, host='0.0.0.0')
--- src/utils.py
+++
@@ -1,6 +0,0 @@
-class Cat:
-    a = 32
-
-
-v = Cat()
-print(v.__dict__)
