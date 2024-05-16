efb5b9890448d2711e44bd6e934e2f560e670fad
SosisterRapStar <timoshenkovvanya@gmail.com>
2024-05-16 17:38:47 +0300

    Connection manager changed

--- src/services/connection_manager.py
+++ src/services/connection_manager.py
@@ -18,102 +18,131 @@ class AbstractConnectionManagersHadler(ABC):
 
 
 
-class ConnectionManagerNotFoundError(Exception):
+
+# class ConnectionManagerNotFoundError(Exception):
+#     def __init__(self, *args):
+#         if args:
+#             self.message = args[0]
+#         else:
+#             self.message = None
+
+#     # ok it should be useful idk :|
+#     def __str__(self):
+#         if self.message:
+#             return f"{self.message}"
+#         return f"There is no such manager"
+class ConnectionWithUserNotFound(Exception):
     def __init__(self, *args):
         if args:
             self.message = args[0]
         else:
             self.message = None
-
-    # ok it should be useful idk :|
     def __str__(self):
         if self.message:
             return f"{self.message}"
-        return f"There is no such manager"
+        return f"NoAnyConnectionsWithUser"
 
 
-class ConversationConnectionManagersHandler(AbstractConnectionManagersHadler):
-    # maybe it should do redis
+# class ConversationConnectionManagersHandler(AbstractConnectionManagersHadler):
+#     # maybe it should do redis
 
-    __singletone = None
+#     __singletone = None
 
-    def __new__(cls, *args, **kwargs):
-        if cls.__singletone is None:
-            cls.__singletone = super().__new__(cls, *args, **kwargs)
-            cls.__managers = dict()
-        return cls.__singletone
+#     def __new__(cls, *args, **kwargs):
+#         if cls.__singletone is None:
+#             cls.__singletone = super().__new__(cls, *args, **kwargs)
+#             cls.__managers = dict()
+#         return cls.__singletone
 
-    def __init__(self):
-        pass
+#     def __init__(self):
+#         pass
         
-    def get_all(self):
-        return self.__managers
-        
-    
-    async def is_conv_registered(self, key: str) -> bool:
-        return key in self.__managers
-    
-    async def registrate_conv(self, key: str) -> "ConnectionManager":
-        manager = ConnectionManager()
-        self.__managers[key] = manager
-        return manager
-    
-    async def get_manager(self, key: str, create_if_no: bool = True) -> "ConnectionManager":
-        if await self.is_conv_registered(key):
-            return self.__managers[key]
-        if create_if_no:
-            return await self.registrate_conv(key)
-        return None # WARNING
+#     def get_all(self):
+#         return self.__managers
         
     
-    async def delete_manager(self, key: str, delete_if_not_empty: bool = False):
-        try:
-            manager: ConnectionManager = self.__managers[key]
-        except LookupError:
-            raise ConnectionManagerNotFoundError()
+#     async def is_conv_registered(self, key: str) -> bool:
+#         return key in self.__managers
+    
+#     async def registrate_conv(self, key: str) -> "ConnectionManager":
+#         manager = ConnectionManager()
+#         self.__managers[key] = manager
+#         return manager
+    
+#     async def get_manager(self, key: str, create_if_no: bool = True) -> "ConnectionManager":
+#         if await self.is_conv_registered(key):
+#             return self.__managers[key]
+#         if create_if_no:
+#             return await self.registrate_conv(key)
+#         return None # WARNING
+        
+    
+#     async def delete_manager(self, key: str, delete_if_not_empty: bool = False):
+#         try:
+#             manager: ConnectionManager = self.__managers[key]
+#         except LookupError:
+#             raise ConnectionManagerNotFoundError()
 
-        if not manager.is_empty:
-            if delete_if_not_empty:
-                await manager.disconnect_all()
-            else:
-                return ...
-        del self.__managers[key]
+#         if not manager.is_empty:
+#             if delete_if_not_empty:
+#                 await manager.disconnect_all()
+#             else:
+#                 return ...
+#         del self.__managers[key]
     
    
 
 class ConnectionManager:
-
+    __singletone = None
+    
+    def __new__(cls, *args, **kwargs):
+        if cls.__singletone is None:
+            cls.__singletone = super().__new__(cls, *args, **kwargs)
+            cls.__users_websockets: dict[uuid.UUID, set[WebSocket]] = dict()
+        return cls.__singletone
+    
     def __init__(self):
-        self.__connections: list[WebSocket] = list()
         self.__counter = 0
         
-
-    async def connect(self, websocket: WebSocket):
+        
+    async def connect(self, user_id: uuid.UUID, websocket: WebSocket):
         await websocket.accept()
         self.__counter += 1
-        self.__connections.append(websocket)
+        
+        if user_id not in self.__users_websockets:
+            self.__users_websockets[user_id] = set()
+        self.__users_websockets[user_id].add(websocket)
+        
+    async def send_to_users(self, users: list[uuid.UUID], message: str):
+        for user_id in users:
+            if user_id in self.users_websockets:
+                for websocket in self.__users_websockets[user_id]:
+                    await websocket.send_text(message)
+            
 
-    def disconnect(self, websocket: WebSocket):
-        self.__connections.remove(websocket)
-        self.__counter -= 1
+    async def disconnect(self, user_id: uuid.UUID, websocket: WebSocket):
+        if user_id in self.__users_websockets:
+            self.__users_websockets[user_id].remove(websocket)
+            self.__counter -= 1
+        else:
+            raise ConnectionWithUserNotFound(f"NoAnyConnectionsWithUser {user_id=}")
 
     async def disconnect_all(self):
-        for socket in self.__connections:
-            await socket.close()
-        self.__connections.clear()
+        for user in self.__users_websockets:
+            for websocket in self.__users_websockets[user]:
+                await websocket.close()
+        self.__users_websockets.clear()
 
-    async def send(self, websocket: WebSocket, message: str):
-        await websocket.send_text(message)
+    # async def send(self, websocket: WebSocket, message: str):
+    #     await websocket.send_text(message)
 
     async def broadcast(self, message: str):
-        for connection in self.__connections:
-            await connection.send_text(message)
+        for user in self.__users_websockets:
+            for websocket in self.__users_websockets[user]:
+                await websocket.send_text(message)
             
-    def get_all(self):
-        return self.__connections
-            
-    def is_empty(self) -> bool:
-        return self.__counter
+    # def is_empty(self) -> bool:
+    #     return self.__counter
             
 
 
