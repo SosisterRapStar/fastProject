81770779338f667578b7a4bfcbd284b10271d0f9
SosisterRapStar <timoshenkovvanya@gmail.com>
2024-06-01 03:24:59 +0300

    fixed typing

--- src/services/redis_service.py
+++ src/services/redis_service.py
@@ -2,7 +2,7 @@ import redis.asyncio as redis
 import asyncio
 from redis import Redis
 from config import settings
-from typing import Any
+from typing import Any, Dict, List
 from src.config import settings
 
 class RedisManager:
@@ -50,23 +50,23 @@ class RedisCache:
         return curr_namespace["ttl"] 
         
         
-    async def get_key(self, key: str) -> any | None:
+    async def get_key(self, key: str) -> Any | None:
         async with self.redis() as r:
             return await r.get(key)
     
         
-    async def set_key(self, key: str, value: str) -> dict[str, str]:
+    async def set_key(self, key: str, value: str) -> Dict[str, str]:
         async with self.redis() as r:
             await r.set(key, value)
             await r.expire(key, self.get_ttl_for_namespace(key=key))
             return {key: value}
     
-    async def set_hset(self, key: str, value: dict) -> None:
+    async def set_hset(self, key: str, value: Dict[str, Any]) -> None:
         async with self.redis() as r:
             await r.hset(key, mapping=value)
             await r.expire(key, self.get_ttl_for_namespace(key=key))
     
-    async def get_hset(self, key: str) -> dict[str, Any] | None:
+    async def get_hset(self, key: str) -> Dict[str, Any] | None:
         async with self.redis() as r:
             value = await r.hgetall(key)
             if value is not None:
@@ -75,12 +75,12 @@ class RedisCache:
             return None # WARN
             
         
-    async def set_list(self, key: str, value: list) -> None:
+    async def set_list(self, key: str, value:  List[Any]) -> None:
         async with self.redis() as r:
             await redis.rpush(key, *value)
             await r.expire(key, self.get_ttl_for_namespace(key=key))
     
-    async def get_list(self, key: str) -> list[Any] | None:
+    async def get_list(self, key: str) -> List[Any] | None:
         async with self.redis() as r:
             res = await r.lrange(key, 0, -1)
             if res is not None:
